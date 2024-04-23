cfcf6b9776a130d1a8d820f702b84e5bebca94a6
JoaquinCarrillo17 <joaquin.carrilloy@um.es>
2024-04-23 10:07:59 +0200

    Historico acabado

--- src/app/demo/historicos/historico/historico.component.ts
+++ src/app/demo/historicos/historico/historico.component.ts
@@ -22,6 +22,7 @@ export default class HistoricoComponent {
   @ViewChild('chart') chart: ChartComponent;
 
   areaAngleChart: Partial<ChartOptions>;
+  habitacionesChart: Partial<ChartOptions>;
   categories: string[] = [];
   hotelesData: number[] = [];
   habitacionesData: number[] = [];
@@ -38,15 +39,10 @@ export default class HistoricoComponent {
 
   loadDataForChart() {
     this.historicoService.getAllHistoricos().subscribe((historicos: Historico[]) => {
-      console.log(historicos);
       historicos.forEach((historico: Historico) => {
         const fecha = new Date(historico.fecha);
-        // Obtener día, mes y año
-        const dia = fecha.getDate();
-        const mes = fecha.getMonth() + 1;
-        const año = fecha.getFullYear();
         // Formatear la fecha en "dd/mm/yyyy"
-        const fechaFormateada = `${dia.toString().padStart(2, '0')}/${mes.toString().padStart(2, '0')}/${año}`;
+        const fechaFormateada = fecha.toLocaleDateString('es-ES');
         this.categories.push(fechaFormateada);
         this.hotelesData.push(historico.hotelesTotales)
         this.habitacionesData.push(historico.habitacionesTotales);
@@ -76,20 +72,47 @@ export default class HistoricoComponent {
             data: this.habitacionesData,
           },
           {
-            name: 'Habitaciones disponibles',
-            data: this.habitacionesDisponiblesData,
-          },
-          {
-            name: 'Habitaciones reservadas',
-            data: this.habitacionesReservadasData,
-          },
-          {
             name: 'Huéspedes',
             data: this.huespedesData,
           },
           {
             name: 'Servicios',
             data: this.serviciosData,
+          },
+        ],
+        xaxis: {
+          categories: this.categories,
+        },
+        tooltip: {
+          followCursor: true,
+        },
+        fill: {
+          opacity: 1,
+        },
+      };
+
+      // Grafico para las habitaciones, habitaciones disponibles y reservadas
+      this.habitacionesChart = {
+        chart: {
+          height: 380,
+          type: 'area',
+          stacked: false,
+        },
+        stroke: {
+          curve: 'straight',
+        },
+        series: [
+          {
+            name: 'Habitaciones',
+            data: this.habitacionesData,
+          },
+          {
+            name: 'Habitaciones Disponibles',
+            data: this.habitacionesDisponiblesData,
+          },
+          {
+            name: 'Habitaciones Reservadas',
+            data: this.habitacionesReservadasData,
           },
         ],
         xaxis: {
